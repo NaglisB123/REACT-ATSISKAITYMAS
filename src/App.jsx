491db230235582080170ac5423672abf9ee2cf5e
NAGLIS BAGDONAS <naglissbagdonass@gmail.com>
2023-09-07 10:40:11 +0300

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import React from 'react';
+
 export default function App() {
   return (
     <div className='container'>
-      <h1>New topic</h1>
+      <h1 className='text-4xl font-bold text-blue-500'>New topic</h1>
     </div>
   );
 }
