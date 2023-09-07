395e3ef93917e495c880668cf7a54add173ee4e7
NAGLIS BAGDONAS <naglissbagdonass@gmail.com>
2023-09-07 14:12:12 +0300

    added products array

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,5 +1,32 @@
 import React from 'react'
 
+const products = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: "$20.00",
+    image: "product1.jpg",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: "$25.00",
+    image: "product2.jpg",
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    price: "$30.00",
+    image: "product3.jpg",
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    price: "$15.00",
+    image: "product4.jpg",
+  },
+];
+
 export default function ProductsPage() {
   return (
     <div>ProductsPage</div>
