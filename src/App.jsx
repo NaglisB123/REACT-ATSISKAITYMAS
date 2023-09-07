1876dae82b2f9af68b505f22e8e127a989927231
NAGLIS BAGDONAS <naglissbagdonass@gmail.com>
2023-09-07 10:45:47 +0300

    added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import React from 'react';
-
 export default function App() {
   return (
     <div className='container'>
-      <h1 className='text-4xl font-bold text-blue-500'>New topic</h1>
+      <h1>New topic</h1>
     </div>
   );
 }
