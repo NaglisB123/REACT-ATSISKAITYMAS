7e84c8485dae5f86040cac2a89266cd4a455a508
NAGLIS BAGDONAS <naglissbagdonass@gmail.com>
2023-09-08 13:32:46 +0300

    auth provider + auth context

---
+++ src/store/AuthProvider.jsx
@@ -0,0 +1,42 @@
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../firebase/firebase";
+
+const AuthContext = createContext({
+  userEmail: "",
+  isLoggedIn: false,
+});
+
+export default function AuthProvider(props) {
+  const [fireUser, setFireUser] = useState(auth.currentUser);
+
+  const userEmail = fireUser?.email;
+  let isLoggedIn = userEmail ? true : false;
+  isLoggedIn = !!userEmail;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const uid = user.uid;
+        console.log("LOGGED IN");
+        setFireUser(user);
+      } else {
+        console.log("LOGGED OUT");
+        setFireUser({});
+      }
+    });
+  }, []);
+
+  const ctx = {
+    userEmail: userEmail,
+    isLoggedIn: isLoggedIn,
+  };
+
+  return (
+    <AuthContext.Provider value={ctx}>{props.children}</AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
