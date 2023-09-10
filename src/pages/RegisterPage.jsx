5bf4e02af040aae72c49334ef434703dab11710a
NAGLIS BAGDONAS <naglissbagdonass@gmail.com>
2023-09-10 13:51:31 +0300

    working register page

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,18 +1,70 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { auth } from "../firebase/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 export default function RegisterPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const navigate = useNavigate();
 
-  const handleRegister = (e) => {
-    e.preventDefault();
-    if (password !== confirmPassword) {
-      alert("Password and confirm password do not match.");
-      return;
-    }
+  const initialValues = {
+    email: "",
+    password: "",
+    confirmPassword: "",
   };
+
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email("Invalid email address")
+      .required("Email is required"),
+    password: Yup.string()
+      .min(6, "Password should be at least 6 characters")
+      .required("Password is required"),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref("password"), null], "Passwords must match")
+      .required("Confirm Password is required"),
+  });
+
+  const [serverError, setServerError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const formik = useFormik({
+    initialValues,
+    validationSchema,
+    onSubmit: (values) => {
+      console.log("Form submitted with values:", values);
+      registerWithFirebase(values.email, values.password);
+    },
+  });
+
+  async function registerWithFirebase(email, password) {
+    try {
+      setIsLoading(true);
+      await createUserWithEmailAndPassword(auth, email, password);
+      loginWithFirebase(email, password);
+    } catch (error) {
+      console.error("Registration error:", error.message);
+      setServerError(error.message);
+    }
+  }
+
+  function loginWithFirebase(email, password) {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log("User authenticated:", user);
+        navigate("/shop-page");
+      })
+      .catch((error) => {
+        const errorMessage = "Email or password is incorrect";
+        console.log("Authentication error:", errorMessage);
+        setServerError(errorMessage);
+      });
+  }
 
   return (
     <div className="flex items-center justify-center">
@@ -20,7 +72,8 @@ export default function RegisterPage() {
         <h2 className="text-center text-3xl font-extrabold text-gray-900 mb-8">
           Register an Account
         </h2>
-        <form onSubmit={handleRegister}>
+        {serverError && <div className="text-red-500">{serverError}</div>}
+        <form onSubmit={formik.handleSubmit}>
           <div className="rounded-md shadow-sm -space-y-px">
             <div className="flex flex-col">
               <input
@@ -31,9 +84,14 @@ export default function RegisterPage() {
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                disabled={isLoading}
               />
+              {formik.touched.email && formik.errors.email ? (
+                <div className="text-red-500">{formik.errors.email}</div>
+              ) : null}
               <input
                 id="password"
                 name="password"
@@ -42,9 +100,14 @@ export default function RegisterPage() {
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                value={formik.values.password}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                disabled={isLoading}
               />
+              {formik.touched.password && formik.errors.password ? (
+                <div className="text-red-500">{formik.errors.password}</div>
+              ) : null}
               <input
                 id="confirmPassword"
                 name="confirmPassword"
@@ -53,19 +116,31 @@ export default function RegisterPage() {
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Confirm Password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
+                value={formik.values.confirmPassword}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                disabled={isLoading}
               />
+              {formik.touched.confirmPassword &&
+              formik.errors.confirmPassword ? (
+                <div className="text-red-500">
+                  {formik.errors.confirmPassword}
+                </div>
+              ) : null}
             </div>
           </div>
 
           <div className="text-center">
-            <button
-              type="submit"
-              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mx-auto"
-            >
-              Register
-            </button>
+            {!isLoading ? (
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mx-auto"
+              >
+                Register
+              </button>
+            ) : (
+              "is loading"
+            )}
           </div>
         </form>
         <div className="text-center mt-4">
