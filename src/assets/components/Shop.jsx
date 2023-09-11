7d9be4ca4e05d09eedae51b4a61e062045818f7b
NAGLIS BAGDONAS <naglissbagdonass@gmail.com>
2023-09-11 11:27:11 +0300

    data render from database

--- src/assets/components/Shop.jsx
+++ src/assets/components/Shop.jsx
@@ -1,12 +1,42 @@
-// Shops.js
-import React from 'react';
+import { db } from "../../firebase/firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
-function Shop() {
+export default function Shop() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [shops, setShops] = useState([]);
+
+  useEffect(() => {
+    getCollection();
+  }, []);
+
+  async function getCollection() {
+    setIsLoading(true);
+    const getData = await getDocs(collection(db, "goods-database"));
+
+    const dataBack = [];
+    getData.forEach((doc) => {
+      dataBack.push({
+        id: doc.id,
+        ...doc.data(),
+      });
+    });
+
+    setShops(dataBack);
+    setIsLoading(false);
+  }
+
   return (
     <div>
-      <h2>Shop Page</h2>
+      <h1>HI</h1>
+      {!isLoading
+        ? shops.map((shop) => (
+            <div key={shop.id}>
+              <div>{shop.shopName}</div>
+              <img src={shop.imagUrl} alt={shop.shopName} />
+            </div>
+          ))
+        : "is loading"}
     </div>
   );
 }
-
-export default Shop;
