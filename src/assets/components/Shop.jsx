0ff4564cbb4f2c2e0d33316cf2ae2336a2e9e1b4
NAGLIS BAGDONAS <naglissbagdonass@gmail.com>
2023-09-11 23:08:21 +0300

    added mapping for cards

--- src/assets/components/Shop.jsx
+++ src/assets/components/Shop.jsx
@@ -27,16 +27,28 @@ export default function Shop() {
   }
 
   return (
-    <div>
-      <h1>HI</h1>
-      {!isLoading
-        ? shops.map((shop) => (
-            <div key={shop.id}>
-              <div>{shop.shopName}</div>
-              <img src={shop.imagUrl} alt={shop.shopName} />
+    <div className="bgBar grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 pl-20">
+      {isLoading ? (
+        <p className="text-center text-gray-500">Loading...</p>
+      ) : (
+        shops.map((shop) => (
+          <div
+            key={shop.id}
+            className="bg-yellow-400 rounded-lg shadow-md p-2 m-6 w-8/12"
+          >
+            <img
+              src={shop.imagUrl}
+              alt={shop.shopName}
+              className="w-full h-48 object-cover rounded-t-lg"
+            />
+            <div className="p-4">
+              <h2 className="text-xl font-semibold">{shop.shopName}</h2>
+              <p className="text-gray-600 italic">{shop.description}$</p>
+              <p>{shop.startYear}</p>
             </div>
-          ))
-        : "is loading"}
+          </div>
+        ))
+      )}
     </div>
   );
 }
